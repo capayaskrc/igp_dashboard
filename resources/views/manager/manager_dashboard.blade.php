8bc8daa46b79a2f76cbba5b1818b15a95392af67
Kayl Rovic Capayas <kayl.capayas@valens-research.com>
2024-04-13 22:46:31 +0800

    added working dashboard admin, auth for every owner,manager, and admin.

--- resources/views/manager/manager_dashboard.blade.php
+++ resources/views/manager/manager_dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="container-fluid mt-5 mx-auto max-w-screen-lg">
         <div class="md:col-span-10 mx-0 px-0">
             <div class="md:flex md:justify-between md:items-center md:space-x-4 mt-4">
-                <div class="md:w-1/2">
+                <div class="md:w-1/2 mt-4 md:mt-0">
                     <div class="bg-white rounded-lg shadow-md">
                         <div class="p-4">
                             <h5 class="text-xl font-semibold mb-2">Owner Management</h5>
@@ -25,7 +25,7 @@
                             <h5 class="text-xl font-semibold mb-2">Rental Management</h5>
                             <hr class="my-2">
                             <p class="text-sm text-gray-700">View and Manage your rentals</p>
-                            <a href="activity_log.php" class="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Go to Rental Management</a>
+                            <a href="{{ route('rental.manage') }}" class="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Go to Rental Management</a>
                         </div>
                     </div>
                 </div>
